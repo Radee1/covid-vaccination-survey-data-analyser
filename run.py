d385990928a2fdcf68607cad48e5856401d833a7
Radee1 <roseadee2013@gmail.com>
2022-04-12 19:25:44 +0000

    import pandas as pd

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # importing the required libraries
-
+import pandas as pd
 # Contants
 """
 This is a welcome message
 """
-WELCOME_MESSAGE = "\nWelcome to the Covid-19 Vaccination Survey Data Analyser! \n"
+WELCOME_MESSAGE = "\nWelcome to the Covid-19 Vaccination Survey Data Analyser!\n"
 TIP1 = "Press 0 if no, 1 if yes and 2 to quit !\n"
 TIP2 = "\nYour name has to be filled in, for you to continue"
 
@@ -38,9 +38,10 @@ def get_name():
     ask = input(TIP1)  # This captures the initial input of the user
 
     if ask != "":  # checks if the user input is not empty
-        if  ask == "0":  # is user refuses to provide name
+        if  ask == "0":  # if user refuses to provide name
             print(TIP2)  
             ask = input(TIP1)
+
         elif ask == "1":  # if answer is yes/ user accepts to provide name
             name = input("\nEnter Your name please!\n")
             print("\nHii "+name+"\n")
@@ -48,7 +49,7 @@ def get_name():
 
             # Analysis criteria
             total_vaccinations = input("\nEnter target number of total vaccinations \n")
-            total_full_vaccinations = input("\nEnter target number of persons fully vaccinations \n")
+            total_full_vaccinations = input("\nEnter target number of persons fully vaccinated \n")
 
             if total_vaccinations != "" and total_full_vaccinations != "":
                limit1 = int(total_vaccinations) #turn str to
@@ -66,13 +67,13 @@ def get_name():
             say("Bye")
 
         elif ask == "2":
-            say("Bye ")
+            say("Bye")
         else:
             print("\nInvalid answer, please try again \n")
             get_name()
     else:
-        print(INFO2)
-        ask = input(INFO1)
+        print(TIP2)
+        ask = input(TIP1)
         get_name()
 say("Hi")
 get_name()
